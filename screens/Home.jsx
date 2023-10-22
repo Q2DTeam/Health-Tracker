22825538a0c0edf3205fa4cecdf1b08303e21ec1
QDev2k3 <ntbquoc2015@gmail.com>
2023-10-23 00:17:45 +0700

    Fix kcalContainer style and position

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -5,6 +5,7 @@ import ProgressCircle from 'react-native-progress-circle'
 // Import components
 import KcalValue from '../components/KcalValue';
 import NutriValue from '../components/NutriValue';
+import MealItem from '../components/MealItem';
 
 const user = {
     totalKcal: 2500,
@@ -22,38 +23,56 @@ export default function Home() {
     return (
         <View style={{backgroundColor: '#F2F5FC', flex: 1}}>
             <StatusBar barStyle="light-content" />
-            <View style={styles.kcalContainer}> 
-                <View style={styles.topBar}></View>
-                <View style={styles.infoContainer}>
-                    <View style={styles.kcalWrapper}>
-                        <KcalValue icon='fire' title='burned' value={user.kcalBurned} />
-                        <ProgressCircle 
-                             percent={Math.round(user.kcalEaten / (user.totalKcal + user.kcalBurned) * 100)}
-                             radius={90}
-                             borderWidth={8}
-                             color="#2ED12E"
-                             shadowColor="#999"
-                             bgColor="#fff"
-                        >
-                            <View style={{alignItems: 'center'}}>
-                                <Text style={styles.remainKcalValue}>{user.totalKcal + user.kcalBurned - user.kcalEaten}</Text>
-                                <Text style={styles.remainKcalText}>Kcal remaining</Text>
+
+            <ScrollView 
+                style={{backgroundColor: '#F2F5FC', flex: 1, borderWidth: 1,}}
+                contentContainerStyle={{alignItems: 'center'}}
+            > 
+                <View style={styles.topBar}>
+                    <View style={styles.kcalContainer}> 
+                        <View style={styles.infoContainer}>
+                            <View style={styles.kcalWrapper}>
+                                <KcalValue icon='fire' title='burned' value={user.kcalBurned} />
+                                <ProgressCircle 
+                                    percent={Math.round(user.kcalEaten / (user.totalKcal + user.kcalBurned) * 100)}
+                                    radius={90}
+                                    borderWidth={8}
+                                    color="#2ED12E"
+                                    shadowColor="#999"
+                                    bgColor="#fff"
+                                >
+                                    <View style={{alignItems: 'center'}}>
+                                        <Text style={styles.remainKcalValue}>{user.totalKcal + user.kcalBurned - user.kcalEaten}</Text>
+                                        <Text style={styles.remainKcalText}>Kcal remaining</Text>
+                                    </View>
+                                </ProgressCircle>
+                                <KcalValue icon='silverware-variant' title='eaten' value={user.kcalEaten} />
                             </View>
-                        </ProgressCircle>
-                        <KcalValue icon='silverware-variant' title='eaten' value={user.kcalEaten} />
-                    </View>
-                    <View style={styles.nutriWrapper}>
-                        <NutriValue title="Carbs" total={user.totalCarbs} consumed={user.carbEaten} />
-                        <NutriValue title="Proteins" total={user.totalProtein} consumed={user.proteinEaten} />
-                        <NutriValue title="Fats" total={user.totalFats} consumed={user.fatEaten} />
+                            <View style={styles.nutriWrapper}>
+                                <NutriValue title="Carbs" total={user.totalCarbs} consumed={user.carbEaten} />
+                                <NutriValue title="Proteins" total={user.totalProtein} consumed={user.proteinEaten} />
+                                <NutriValue title="Fats" total={user.totalFats} consumed={user.fatEaten} />
+                            </View>
+                        </View>
                     </View>
                 </View>
-            </View>
+
+                <View style={styles.homeBody}>
+                    
+                </View>
+            </ScrollView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    topBar: {
+        height: 400,
+        width: '100%',
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
     kcalContainer: { // The white circle
         backgroundColor: '#fff',
         width: 700,
@@ -61,14 +80,10 @@ const styles = StyleSheet.create({
         borderRadius: 350,
         borderColor: '#E0E6F3',
         borderWidth: 3,
-        position: 'absolute',
-        top: '-45%',
-        left: '-35%',
         paddingTop: 400,
         alignItems: 'center',
     },
     kcalWrapper: {
-        //borderWidth: 1,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -88,10 +103,15 @@ const styles = StyleSheet.create({
 
     // Nutrients Values
     nutriWrapper: {
-        //borderWidth: 1,
         marginTop: 20,
         width: 375,
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+
+    homeBody: {
+        marginTop: 10,
+        borderWidth: 1,
+        flex: 1,
     },
 });
