aa3e1630006ec12f8a2bc349651185244072c93e
QDev2k3 <ntbquoc2015@gmail.com>
2023-10-23 10:05:46 +0700

    BMI component

--- global/styles.js
+++ global/styles.js
@@ -4,4 +4,20 @@ export const globalColors = {
     'lunchOrange': '#FFA935',
     'dinnerCyan': '#12E5B0',
     'snackPurple': '#B575E7',
+    'backgroundGray': '#F2F5FC',
+    'textGray': '#9EA9C3',
 }
+
+/*
+    Normal Shadow:
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+
+*/
--- screens/Me.jsx
+++ screens/Me.jsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import styles
+import { globalColors } from '../global/styles';
+
+// Import components
+import BMI from '../components/BMI';
+
 
 export default function Me() {
     return (
-        <View>
-            <Text>Me</Text>
+        <View style={{flex: 1, backgroundColor: globalColors.backgroundGray}}>
+            <StatusBar barStyle="light-content" />
+            <View style={styles.profileBody}>
+                <View style={{marginVertical: 20,}}>
+                    <Text style={styles.bmiTitle}>BMI</Text>
+                    <BMI />
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    profileBody: {
+        flex: 1, 
+        marginTop: 24,
+        alignItems: 'center',
+    },
+    bmiTitle: {
+        fontSize: 20,
+        marginBottom: 10,
+    }
+});
