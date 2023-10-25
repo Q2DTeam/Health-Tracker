1ad5b8b6a5a4316e1b6baa33f0b8245ba331ffc2
QDev2k3 <ntbquoc2015@gmail.com>
2023-10-25 23:04:53 +0700

    Navigation overhaul

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,117 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+// The Home screen, used for navigation of screens
+
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import ProgressCircle from 'react-native-progress-circle'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Import components
-import KcalValue from '../components/KcalValue';
-import NutriValue from '../components/NutriValue';
-import MealItem from '../components/MealItem';
+const Stack = createNativeStackNavigator();
 
-const user = {
-    totalKcal: 2500,
-    totalCarbs: 257,
-    totalProtein: 257,
-    totalFats: 98,
-    kcalBurned: 1000,
-    kcalEaten: 1400,
-    carbEaten: 100,
-    proteinEaten: 100,
-    fatEaten: 100,
-}
+
+// Import Stack screens
+import HomeMain from './HomeMain';
+import AddMeal from './AddMeal';
+
 
 export default function Home() {
     return (
-        <View style={{backgroundColor: '#F2F5FC', flex: 1}}>
-            <StatusBar barStyle="light-content" />
-
-            <ScrollView 
-                style={{backgroundColor: '#F2F5FC', flex: 1, borderWidth: 1,}}
-                contentContainerStyle={{alignItems: 'center'}}
-            > 
-                <View style={styles.topBar}>
-                    <View style={styles.kcalContainer}> 
-                        <View style={styles.infoContainer}>
-                            <View style={styles.kcalWrapper}>
-                                <KcalValue icon='fire' title='burned' value={user.kcalBurned} />
-                                <ProgressCircle 
-                                    percent={Math.round(user.kcalEaten / (user.totalKcal + user.kcalBurned) * 100)}
-                                    radius={90}
-                                    borderWidth={8}
-                                    color="#2ED12E"
-                                    shadowColor="#999"
-                                    bgColor="#fff"
-                                >
-                                    <View style={{alignItems: 'center'}}>
-                                        <Text style={styles.remainKcalValue}>{user.totalKcal + user.kcalBurned - user.kcalEaten}</Text>
-                                        <Text style={styles.remainKcalText}>Kcal remaining</Text>
-                                    </View>
-                                </ProgressCircle>
-                                <KcalValue icon='silverware-variant' title='eaten' value={user.kcalEaten} />
-                            </View>
-                            <View style={styles.nutriWrapper}>
-                                <NutriValue title="Carbs" total={user.totalCarbs} consumed={user.carbEaten} />
-                                <NutriValue title="Proteins" total={user.totalProtein} consumed={user.proteinEaten} />
-                                <NutriValue title="Fats" total={user.totalFats} consumed={user.fatEaten} />
-                            </View>
-                        </View>
-                    </View>
-                </View>
-
-                <View style={styles.homeBody}>
-                    
-                </View>
-            </ScrollView>
-        </View>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen 
+                name="HomeMain" 
+                component={HomeMain}
+                options={homeOptions} />
+            <Stack.Screen name="AddMeal" component={AddMeal} />
+        </Stack.Navigator>
     )
 }
 
 const styles = StyleSheet.create({
-    topBar: {
-        height: 400,
-        width: '100%',
-        overflow: 'hidden',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-    },
-    kcalContainer: { // The white circle
-        backgroundColor: '#fff',
-        width: 700,
-        height: 700,
-        borderRadius: 350,
-        borderColor: '#E0E6F3',
-        borderWidth: 3,
-        paddingTop: 400,
-        alignItems: 'center',
-    },
-    kcalWrapper: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        height: 175,
-        width: 375,
-    },
-    remainKcalValue: {
-        fontSize: 36,
-        color: "#2ED12E",
-        fontFamily: 'inter-bold',
-    },
-    remainKcalText: {
-        fontSize: 12,
-        color: "#9DA8C3",
-        fontFamily: 'inter-regular',
-    },
 
-    // Nutrients Values
-    nutriWrapper: {
-        marginTop: 20,
-        width: 375,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-
-    homeBody: {
-        marginTop: 10,
-        borderWidth: 1,
-        flex: 1,
-    },
 });
+
+const homeOptions = {
+    headerShown: false,
+};
---
+++ screens/HomeMain.jsx
@@ -0,0 +1,128 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import ProgressCircle from 'react-native-progress-circle';
+
+
+// Import components
+import KcalValue from '../components/KcalValue';
+import NutriValue from '../components/NutriValue';
+import MealItem from '../components/MealItem';
+import ActionButton from '../components/ActionButton';
+
+
+const user = {
+    totalKcal: 3500,
+    totalCarbs: 257,
+    totalProtein: 257,
+    totalFats: 98,
+    kcalBurned: 1000,
+    kcalEaten: 1400,
+    carbEaten: 100,
+    proteinEaten: 100,
+    fatEaten: 100,
+}
+
+export default function HomeMain({ navigation }) {
+
+    const handleNavigation = (name) => {
+        navigation.navigate('AddMeal');
+    }
+
+    return (
+        <View style={{backgroundColor: '#F2F5FC', flex: 1}}>
+            <StatusBar barStyle="light-content" />
+            <ScrollView 
+                style={{backgroundColor: '#F2F5FC', flex: 1}}
+                contentContainerStyle={{alignItems: 'center'}}
+            > 
+                <View style={styles.topBar}>
+                    <View style={styles.kcalContainer}> 
+                        <View style={styles.infoContainer}>
+                            <View style={styles.kcalWrapper}>
+                                <KcalValue icon='fire' title='burned' value={user.kcalBurned} />
+                                <ProgressCircle 
+                                    percent={Math.round(user.kcalEaten / (user.totalKcal + user.kcalBurned) * 100)}
+                                    radius={90}
+                                    borderWidth={8}
+                                    color="#2ED12E"
+                                    shadowColor="#999"
+                                    bgColor="#fff"
+                                >
+                                    <View style={{alignItems: 'center'}}>
+                                        <Text style={styles.remainKcalValue}>{user.totalKcal + user.kcalBurned - user.kcalEaten}</Text>
+                                        <Text style={styles.remainKcalText}>Kcal remaining</Text>
+                                    </View>
+                                </ProgressCircle>
+                                <KcalValue icon='silverware-variant' title='eaten' value={user.kcalEaten} />
+                            </View>
+                            <View style={styles.nutriWrapper}>
+                                <NutriValue title="Carbs" total={user.totalCarbs} consumed={user.carbEaten} />
+                                <NutriValue title="Proteins" total={user.totalProtein} consumed={user.proteinEaten} />
+                                <NutriValue title="Fats" total={user.totalFats} consumed={user.fatEaten} />
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                
+                <View style={styles.homeBody}>
+                    <MealItem type='breakfast' />
+                    <MealItem type='lunch' />
+                    <MealItem type='dinner' />
+                    <MealItem type='snack' />
+                </View>
+            </ScrollView>
+            <ActionButton handleNavigation={handleNavigation} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    topBar: {
+        height: 400,
+        width: '100%',
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    kcalContainer: { // The white circle
+        backgroundColor: '#fff',
+        width: 700,
+        height: 700,
+        borderRadius: 350,
+        borderColor: '#E0E6F3',
+        borderWidth: 3,
+        paddingTop: 400,
+        alignItems: 'center',
+    },
+    kcalWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 175,
+        width: 375,
+    },
+    remainKcalValue: {
+        fontSize: 36,
+        color: "#2ED12E",
+        fontFamily: 'inter-bold',
+    },
+    remainKcalText: {
+        fontSize: 12,
+        color: "#9DA8C3",
+        fontFamily: 'inter-regular',
+    },
+
+    // Nutrients Values
+    nutriWrapper: {
+        marginTop: 20,
+        width: 375,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    homeBody: {
+        flex: 1,
+        marginVertical: 10,
+        height: '100%',
+    },
+});
