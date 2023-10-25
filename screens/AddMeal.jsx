ca876c40f7906a2c694edb88fc3ae9d6f161ba00
QDev2k3 <ntbquoc2015@gmail.com>
2023-10-25 23:28:18 +0700

    Add screen code

--- screens/AddMeal.jsx
+++ screens/AddMeal.jsx
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+// Import styles
+import { globalColors, globalStyles } from '../global/styles';
 
 
-export default function AddMeal() {
+function Header({ title='breakfast', backFunc }) {
     return (
-        <View>
-            
-            <Text>THIS IS ADDMEAL SCREEN</Text>
+        <View style={globalStyles.header}>
+            <View style={styles.top}>
+                <TouchableOpacity style={globalStyles.backButton} onPress={backFunc}>
+                    <AntDesign name='left' size={30} />
+                </TouchableOpacity>
+                <Text style={globalStyles.headerTitle}>{title[0].toUpperCase() + title.slice(1)}</Text>
+                <Text style={styles.kcalText}>{0} kcal</Text>
+            </View>
         </View>
     )
 }
+
+export default function AddMeal({ navigation }) {
+    const goBack = () => {
+        navigation.goBack();
+    }
+
+    return (
+        <View style={globalStyles.container}>
+            <Header backFunc={goBack} />
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    top: {
+        //borderWidth: 1,
+        margin: 10,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    kcalText: {
+        color: globalColors.textGray,
+        fontSize: 16,
+    }
+});
