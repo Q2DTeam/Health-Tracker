a69f8a97144f0fe19c4ffd75e4b52b3f5ade6310
QDev2k3 <ntbquoc2015@gmail.com>
2023-10-25 23:28:44 +0700

    Add globalStyles

--- global/styles.js
+++ global/styles.js
@@ -10,6 +10,28 @@ export const globalColors = {
     'textGray': '#9EA9C3',
 }
 
-const globalStyles = StyleSheet.create({
-    
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 24,
+        backgroundColor: globalColors.backgroundGray,
+    },
+    header: {
+        width: '100%',
+        height: 100,
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+    backButton: {
+        width: 40,
+        height: 40,
+        backgroundColor: globalColors.backgroundGray,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerTitle: {
+        fontSize: 20,
+    },
 });
