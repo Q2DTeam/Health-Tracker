47ceefcbb58bbb298068f901e9e1084cad5d98b6
Dang <21520683@gm.uit.edu.vn>
2023-12-22 22:24:25 +0700

    list color global

--- global/others.js
+++ global/others.js
@@ -5,35 +5,16 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import moment from 'moment'; 
 //Create michellanious
 export const michellanious = StyleSheet.create({
-    wheelContainer: {
-        width: 334,
-        height: 180,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    details: {
-        justifyContent: 'space-around',
-        height: 120,
-    },
-    infoItem: {
-        minWidth: 100,
-        flexDirection: 'row',
-    },
-    infoName: {
-        marginLeft: 10,
-        fontSize: 16,
-    },
-});
+    'vibrantBlue': '#0099FF',
+    'breakfastGreen': '#2ED12E',
+    'lunchOrange': '#FFA935',
+    'dinnerCyan': '#12E5B0',
+    'snackPurple': '#B575E7',
+    'backgroundGray': '#F2F5FC',
+    'textGray': '#9EA9C3',
+    'chillGreen': '#91C788',
+    'chillCyan': '#2FC9EF',
+    'darkerGreen': '#72B766',
+    'darkerCyan': '#42cd8c',
+    'backgroundCyan': '#60dda3',
+    'calmRed': '#FF5F79'});
