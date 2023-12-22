46df1379193af578cc916a26bc22ae9669d88858
Dang <21520683@gm.uit.edu.vn>
2023-12-22 22:28:04 +0700

    delete function in others.js

--- global/others.js
+++ global/others.js
@@ -1,17 +1,5 @@
 import { StyleSheet } from "react-native";
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 //Create michellanious
-export const michellanious = function KcalValue({icon, title, value}) {
-    return (
-        <View style={styles.kcalWrapper}>
-            <MaterialCommunityIcons 
-                name={icon} 
-                size={30}
-                color={icon === 'fire' ? '#FFA935' : '#B575E7'}
-            />
-            <Text style={styles.kcalValue}>{value}</Text>
-            <Text style={styles.kcalText}>{title}</Text>
-        </View>
-    )
-}
+export const michellanious = StyleSheet.create({
+    
+});
