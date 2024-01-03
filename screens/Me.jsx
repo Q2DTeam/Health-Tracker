fd5a832a72f6287e87ac62ff576c90df00fadbd4
Dang <21520683@gm.uit.edu.vn>
2024-01-03 23:18:11 +0700

    Check

--- screens/Me.jsx
+++ screens/Me.jsx
@@ -298,17 +298,7 @@ const styles = StyleSheet.create({
         minHeight: 140,
         backgroundColor: '#fff',
         borderRadius: 10,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
 
-        elevation: 5,
     },
     bmiHalf: {
         width: '100%',
