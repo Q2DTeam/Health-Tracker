4de35ea6bf1457248a968f210bb3e262e532af4a
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-01-04 10:11:54 +0700

    Remove config

--- utils/firebase.js
+++ utils/firebase.js
@@ -5,13 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSQMJ6JAY1g1rD09pyC1M-EhbsjK06p4U",
-  authDomain: "health-trackr-214d0.firebaseapp.com",
-  projectId: "health-trackr-214d0",
-  storageBucket: "health-trackr-214d0.appspot.com",
-  messagingSenderId: "353638544673",
-  appId: "1:353638544673:web:bb12a6e1fa768219e6ba60",
-  measurementId: "G-J3GK85J74W"
+  // Firebase app config
 };
 
 let app, auth;
